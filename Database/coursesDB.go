8662f6b5ca09e77b272c7450b757546557d8a157
oojas <oojasgupta@gmail.com>
2022-04-11 16:22:17 +0530

    New jobs added

--- Database/coursesDB.go
+++ Database/coursesDB.go
@@ -21,6 +21,8 @@ func GetDataJobs() []Modals.Jobs {
 		{"TCS", "TCS Digital Hiring", imageProcessing.ImageToBase64("images/tcs.png"), "https://jobforfreshar.in/index.php/tcs-recruitment-2022-for-digital-hiring/"},
 		{"Wipro", "Associate", imageProcessing.ImageToBase64("images/wipro.jpg"), "https://bit.ly/3NoOz1Q "},
 		{"Tata Steel", "Engineer Trainee", imageProcessing.ImageToBase64("images/tata.png"), "https://alljobsinindia.in/tata-steel-recruitment-2022/"},
+		{"Eloelo", "Android developer", imageProcessing.ImageToBase64("images/eloelo.jpg"), "https://tinyurl.com/5xe32nke"},
+		{"Tata Communications", "Software Engineer", imageProcessing.ImageToBase64("images/tata-comm.jpg"), "https://alljobsinindia.in/tata-communications-recruitment/"},
 	}
 	return jobs
 }
